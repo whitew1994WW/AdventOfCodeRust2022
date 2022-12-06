62b5a4622d07fb166fc5309fc1bef967b7068e18
William White <williamwhite@Williams-MacBook-Pro.local>
2022-12-06 18:22:41 +0000

    Removed target folder

--- day_1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/Volumes/Crucial\ X6/Github\ Projects/rust_advent_of_code/day_1/target/debug/day1: /Volumes/Crucial\ X6/Github\ Projects/rust_advent_of_code/day_1/src/main.rs
--- day_1/target/debug/deps/day1-a6c91060de33eef8.d
+++
@@ -1,5 +0,0 @@
-/Volumes/Crucial X6/Github Projects/rust_advent_of_code/day_1/target/debug/deps/day1-a6c91060de33eef8: src/main.rs
-
-/Volumes/Crucial X6/Github Projects/rust_advent_of_code/day_1/target/debug/deps/day1-a6c91060de33eef8.d: src/main.rs
-
-src/main.rs:
